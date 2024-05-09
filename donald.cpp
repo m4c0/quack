e73cdb6b2ce641e9ba73a32665df6c3e594fbf72
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-09 11:39:39 +0100

    donald: simpler init with atlas/batch init check

--- donald.cpp
+++ donald.cpp
@@ -10,34 +10,20 @@ import voo;
 static const char *g_app_name = "app";
 static unsigned g_max_quads = 0;
 static quack::upc g_upc{};
-static quack::donald::atlas_fn g_atlas{};
-static quack::donald::data_fn g_data{};
+static quack::donald::atlas_fn g_atlas_fn{};
+static quack::donald::data_fn g_data_fn{};
 
 // TODO: sync count change with data change
 static unsigned g_quads = 0;
+static quack::donald::atlas_t *g_atlas;
+static quack::instance_batch_thread *g_batch;
 
-static void update(quack::mapped_buffers all) { g_quads = g_data(all); }
+static void update(quack::mapped_buffers all) { g_quads = g_data_fn(all); }
 
 namespace {
 class thread : public voo::casein_thread {
-  friend class ibt;
-  quack::donald::atlas_t *m_atlas;
-  quack::instance_batch_thread *m_batch;
-
-protected:
-  using atlas = hai::uptr<voo::updater<voo::h2l_image>>;
-
 public:
   void run() override;
-
-  void refresh_atlas() {
-    wait_init();
-    m_atlas->run_once();
-  }
-  void refresh_batch() {
-    wait_init();
-    m_batch->run_once();
-  }
 };
 } // namespace
 
@@ -45,14 +31,14 @@ void thread::run() {
   voo::device_and_queue dq{g_app_name};
   quack::pipeline_stuff ps{dq, 1};
 
-  hai::uptr atlas{g_atlas(&dq)};
+  hai::uptr atlas{g_atlas_fn(&dq)};
   atlas->run_once();
-  m_atlas = &*atlas;
+  g_atlas = &*atlas;
 
   quack::instance_batch_thread ib{dq.queue(), ps.create_batch(g_max_quads),
                                   update};
   ib.run_once();
-  m_batch = &ib;
+  g_batch = &ib;
 
   auto smp = vee::create_sampler(vee::nearest_sampler);
   auto dset = ps.allocate_descriptor_set(atlas->data().iv(), *smp);
@@ -77,11 +63,30 @@ void thread::run() {
   }
 }
 
+static thread &t() {
+  static thread i{};
+  return i;
+}
+
 namespace quack::donald {
 void app_name(const char *n) { g_app_name = n; }
 void max_quads(unsigned q) { g_max_quads = q; }
 
 void push_constants(quack::upc u) { g_upc = u; }
-void atlas(atlas_fn a) { g_atlas = a; }
-void data(data_fn d) { g_data = d; }
+void atlas(atlas_fn a) {
+  g_atlas_fn = a;
+  if (g_atlas) {
+    g_atlas->run_once();
+  } else if (!g_batch) {
+    t();
+  }
+}
+void data(data_fn d) {
+  g_data_fn = d;
+  if (g_batch) {
+    g_batch->run_once();
+  } else if (!g_atlas) {
+    t();
+  }
+}
 } // namespace quack::donald
