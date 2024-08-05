76031a4b50360d8108a799ae7b24021a0f1e0e1f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-05 14:25:33 +0100

    wasm: local storage for instances

--- donald_wasm.cpp
+++ donald_wasm.cpp
@@ -1,5 +1,6 @@
 module quack;
 import dotz;
+import hai;
 import jute;
 import vaselin;
 
@@ -8,12 +9,12 @@ extern "C"
 void clear_colour(int r, int g, int b, int a);
 
 static const char *g_app_name = "app";
-static unsigned g_max_quads = 0;
+static hai::array<quack::instance> g_quads{};
 static quack::upc g_upc{};
 
 namespace quack::donald {
 void app_name(const char *n) { g_app_name = n; }
-void max_quads(unsigned q) { g_max_quads = q; }
+void max_quads(unsigned q) { g_quads.set_capacity(q); }
 
 void clear_colour(dotz::vec4 c) { 
   c = c * 256;
@@ -24,5 +25,7 @@ void push_constants(quack::upc u) { g_upc = u; }
 void atlas(jute::view res_name) {}
 void atlas(const void *ptr, unsigned width, unsigned height) {}
 
-void data(data_fn d) {}
+void data(data_fn d) {
+  auto n = d(g_quads.begin());
+}
 } // namespace quack::donald
