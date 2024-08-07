9431632cbc318c627bf1a9a7f4627c724eeaa07c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-07 10:55:35 +0100

    wasm: minor reformat

--- donald_wasm.js
+++ donald_wasm.js
@@ -1,4 +1,4 @@
-(function() {
+!function() {
   const canvas = document.getElementById("casein-canvas");
   const ctx = canvas.getContext("2d");
 
@@ -7,13 +7,11 @@
       const colour = `rgba(${r}, ${g}, ${b}, ${a})`;
       canvas.style.backgroundColor = colour;
     },
-    fill_colour : (r, g, b, a) => {
-      ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
-    },
+    fill_colour : (r, g, b, a) => ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`,
     fill_rect : (x, y, w, h) => ctx.fillRect(x, y, w, h),
     restore : () => ctx.restore(),
     save : () => ctx.save(),
     scale : (x, y) => ctx.scale(x, y),
     translate : (x, y) => ctx.translate(x, y),
   };
-}());
+}();
