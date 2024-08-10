680b100a462fc18ca379fdce0653a705c94cc6b9
Eduardo <m4c0@users.noreply.github.com>
2024-08-10 11:49:04 +0100

    wasm: reformat

--- donald_wasm.cpp
+++ donald_wasm.cpp
@@ -1,11 +1,11 @@
 module quack;
+import casein;
 import dotz;
 import hai;
 import jute;
 import vaselin;
 
-#define IMPORT(R, N)                                                           \
-  extern "C" [[clang::import_module("quack"), clang::import_name(#N)]] R N
+#define IMPORT(R, N) extern "C" [[clang::import_module("quack"), clang::import_name(#N)]] R N
 
 IMPORT(void, clear)();
 IMPORT(void, clear_colour)(int r, int g, int b, int a);
@@ -17,48 +17,48 @@ IMPORT(void, save)();
 IMPORT(void, scale)(float, float);
 IMPORT(void, translate)(float, float);
 
-static const char *g_app_name = "app";
-static hai::array<quack::instance> g_quads{};
-static quack::upc g_upc{};
+static const char * g_app_name = "app";
+static hai::array<quack::instance> g_quads {};
+static quack::upc g_upc {};
 
 namespace quack::donald {
-void app_name(const char *n) { g_app_name = n; }
-void max_quads(unsigned q) { g_quads.set_capacity(q); }
+  void app_name(const char * n) { g_app_name = n; }
+  void max_quads(unsigned q) { g_quads.set_capacity(q); }
 
-void clear_colour(dotz::vec4 c) {
-  c = c * 256;
-  ::clear_colour(c.x, c.y, c.z, c.w);
-}
-void push_constants(quack::upc u) { g_upc = u; }
+  void clear_colour(dotz::vec4 c) {
+    c = c * 256;
+    ::clear_colour(c.x, c.y, c.z, c.w);
+  }
+  void push_constants(quack::upc u) { g_upc = u; }
 
-void atlas(jute::view res_name) {}
-void atlas(const void *ptr, unsigned width, unsigned height) {}
+  void atlas(jute::view res_name) {}
+  void atlas(const void * ptr, unsigned width, unsigned height) {}
 
-void data(data_fn d) {
-  auto i = g_quads.begin();
-  auto qty = d(i);
-
-  save();
-  clear();
-
-  auto s = dotz::vec2{800, 600} / (g_upc.grid_size * 4.0f);
-  scale(s.x, s.y);
-  translate(-1 + g_upc.grid_pos.x, -2 + g_upc.grid_pos.y);
-
-  for (auto n = 0; n < qty; n++, i++) {
-    fill_colour(i->colour.x, i->colour.y, i->colour.z, i->colour.w);
+  void data(data_fn d) {
+    auto i = g_quads.begin();
+    auto qty = d(i);
 
     save();
-    auto pivot = i->position + dotz::vec2{i->rotation.rel_x, i->rotation.rel_y};
-    translate(pivot.x, pivot.y);
-    rotate(i->rotation.angle);
-    translate(-pivot.x, -pivot.y);
+    clear();
 
-    fill_rect(i->position.x, i->position.y, i->size.x, i->size.y);
+    auto s = dotz::vec2 { 800, 600 } / (g_upc.grid_size * 4.0f);
+    scale(s.x, s.y);
+    translate(-1 + g_upc.grid_pos.x, -2 + g_upc.grid_pos.y);
+
+    for (auto n = 0; n < qty; n++, i++) {
+      fill_colour(i->colour.x, i->colour.y, i->colour.z, i->colour.w);
+
+      save();
+      auto pivot = i->position + dotz::vec2 { i->rotation.rel_x, i->rotation.rel_y };
+      translate(pivot.x, pivot.y);
+      rotate(i->rotation.angle);
+      translate(-pivot.x, -pivot.y);
+
+      fill_rect(i->position.x, i->position.y, i->size.x, i->size.y);
+
+      restore();
+    }
 
     restore();
   }
-
-  restore();
-}
 } // namespace quack::donald
