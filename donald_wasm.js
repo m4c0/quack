e47cc331dc0a7e05e2b4c97af06137567457d631
Eduardo <m4c0@users.noreply.github.com>
2024-08-12 00:59:53 +0100

    wasm: mag filter

--- donald_wasm.js
+++ donald_wasm.js
@@ -156,6 +156,7 @@
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
       };
       img.src = vaselin_tostr(ptr, sz);
     },
