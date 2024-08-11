d799f549943678155625941f59ab01ea0d0b691a
Eduardo <m4c0@users.noreply.github.com>
2024-08-12 00:57:31 +0100

    wasm: using NEAREST filter

--- donald_wasm.js
+++ donald_wasm.js
@@ -155,7 +155,7 @@
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
       };
       img.src = vaselin_tostr(ptr, sz);
     },
