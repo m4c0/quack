b44a59d76dbcebe812e1435bf14749bd22a59d63
Eduardo <m4c0@users.noreply.github.com>
2024-08-24 15:32:53 +0100

    donald/wasm: avoid naming conflicts

--- donald_wasm.cpp
+++ donald_wasm.cpp
@@ -6,32 +6,33 @@ import jute;
 import sires;
 import vaselin;
 
-#define IMPORT(R, N) extern "C" [[clang::import_module("quack_donald"), clang::import_name(#N)]] R N
-
-IMPORT(void, clear_colour)(float, float, float, float);
-IMPORT(void, bind_instances)(unsigned, const void *, unsigned);
-IMPORT(void, load_texture)(const char *, unsigned);
-IMPORT(void, set_grid)(float, float, float, float);
-IMPORT(void, start)();
+#define IMPORT(R, N) [[clang::import_module("quack_donald"), clang::import_name(#N)]] extern R N
 
 static const char * g_app_name = "app";
 static hai::array<quack::instance> g_quads {};
 static quack::upc g_upc {};
 static bool g_started {};
 
+namespace quack::donald::js {
+  IMPORT(void, clear_colour)(float, float, float, float);
+  IMPORT(void, bind_instances)(unsigned, const void *, unsigned);
+  IMPORT(void, load_texture)(const char *, unsigned);
+  IMPORT(void, set_grid)(float, float, float, float);
+  IMPORT(void, start)();
+}
 namespace quack::donald {
   void app_name(const char * n) { g_app_name = n; }
   void max_quads(unsigned q) { g_quads.set_capacity(q); }
 
   void clear_colour(dotz::vec4 c) {
     c = dotz::pow(c, 1.0 / 2.2);
-    ::clear_colour(c.x, c.y, c.z, c.w);
+    js::clear_colour(c.x, c.y, c.z, c.w);
   }
   void push_constants(quack::upc u) {
     auto aspect = casein::window_size.x / casein::window_size.y;
     u = quack::adjust_aspect(u, aspect);
 
-    set_grid(u.grid_pos.x, u.grid_pos.y, u.grid_size.x, u.grid_size.y);
+    js::set_grid(u.grid_pos.x, u.grid_pos.y, u.grid_size.x, u.grid_size.y);
     g_upc = u;
   }
 
@@ -41,7 +42,7 @@ namespace quack::donald {
     return rel * g_upc.grid_size + g_upc.grid_pos;
   }
 
-  void atlas(jute::view res_name) { load_texture(res_name.begin(), res_name.size()); }
+  void atlas(jute::view res_name) { js::load_texture(res_name.begin(), res_name.size()); }
   void atlas(const void * ptr, unsigned width, unsigned height) {}
 
   void data(buffer_fn_t d) {
@@ -49,10 +50,10 @@ namespace quack::donald {
     d(i);
     auto qty = i - g_quads.begin();
 
-    if (qty > 0) bind_instances(qty, g_quads.begin(), g_quads.size());
+    if (qty > 0) js::bind_instances(qty, g_quads.begin(), g_quads.size());
 
     if (!g_started) {
-      start();
+      js::start();
       g_started = true;
     }
   }
