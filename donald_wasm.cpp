ae071689745b2caf7f22a456b4b9d3ef18d3a576
Eduardo <m4c0@users.noreply.github.com>
2024-09-01 23:04:42 +0100

    donald/wasm: migrate to gelo

--- donald_wasm.cpp
+++ donald_wasm.cpp
@@ -1,6 +1,8 @@
 module quack;
+import :wasm;
 import casein;
 import dotz;
+import gelo;
 import hai;
 import jute;
 import sires;
@@ -13,26 +15,36 @@ static hai::array<quack::instance> g_quads {};
 static quack::upc g_upc {};
 static bool g_started {};
 
-namespace quack::donald::js {
-  IMPORT(void, clear_colour)(float, float, float, float);
-  IMPORT(void, bind_instances)(unsigned, const void *, unsigned);
-  IMPORT(void, load_texture)(const char *, unsigned);
-  IMPORT(void, set_grid)(float, float, float, float);
-  IMPORT(void, start)();
+static int g_u_pos;
+static int g_u_size;
+static int g_buffer;
+static int g_texture;
+static unsigned g_qty {};
+
+static jute::view g_atlas {};
+
+static void draw(void *) {
+  quack::wasm::clear();
+  gelo::draw_arrays_instanced(gelo::TRIANGLES, 0, 6, g_qty);
+  vaselin::request_animation_frame(draw, nullptr);
 }
+static void load_atlas(void *) {
+  quack::wasm::load_texture(g_texture, g_atlas); 
+}
+
 namespace quack::donald {
   void app_name(const char * n) { g_app_name = n; }
   void max_quads(unsigned q) { g_quads.set_capacity(q); }
 
   void clear_colour(dotz::vec4 c) {
     c = dotz::pow(c, 1.0 / 2.2);
-    js::clear_colour(c.x, c.y, c.z, c.w);
+    gelo::clear_color(c.x, c.y, c.z, c.w);
   }
   void push_constants(quack::upc u) {
     auto aspect = casein::window_size.x / casein::window_size.y;
     u = quack::adjust_aspect(u, aspect);
-
-    js::set_grid(u.grid_pos.x, u.grid_pos.y, u.grid_size.x, u.grid_size.y);
+    gelo::uniform2f(g_u_pos, u.grid_pos.x, u.grid_pos.y);
+    gelo::uniform2f(g_u_size, u.grid_size.x, u.grid_size.y);
     g_upc = u;
   }
 
@@ -42,19 +54,32 @@ namespace quack::donald {
     return rel * g_upc.grid_size + g_upc.grid_pos;
   }
 
-  void atlas(jute::view res_name) { js::load_texture(res_name.begin(), res_name.size()); }
+  void atlas(jute::view res_name) { 
+    g_atlas = res_name;
+    vaselin::request_animation_frame(load_atlas, nullptr);
+  }
   void atlas(const void * ptr, unsigned width, unsigned height) {}
 
   void data(buffer_fn_t d) {
+    if (!g_started) {
+      auto [ prog, b ] = wasm::setup();
+      g_u_pos = gelo::get_uniform_location(prog, "pc.grid_pos");
+      g_u_size = gelo::get_uniform_location(prog, "pc.grid_size");
+      g_buffer = wasm::create_buffer();
+      g_texture = wasm::create_texture();
+      g_started = true;
+
+      vaselin::request_animation_frame(draw, nullptr);
+    }
+
     auto i = g_quads.begin();
     d(i);
     auto qty = i - g_quads.begin();
 
-    if (qty > 0) js::bind_instances(qty, g_quads.begin(), g_quads.size());
-
-    if (!g_started) {
-      js::start();
-      g_started = true;
+    if (qty > 0) {
+      gelo::bind_buffer(gelo::ARRAY_BUFFER, g_buffer);
+      gelo::buffer_data(gelo::ARRAY_BUFFER, g_quads.begin(), g_quads.size(), gelo::STATIC_DRAW);
+      g_qty = qty;
     }
   }
 } // namespace quack::donald
